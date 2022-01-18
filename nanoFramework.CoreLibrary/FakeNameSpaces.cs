7f85765e849698fbda6447bb2b9d79621417b741
josesimoes <jose.simoes@eclo.solutions>
2022-01-18 18:28:37 +0000

    Need these dummy namespaces to have

---
+++ nanoFramework.CoreLibrary/FakeNameSpaces.cs
@@ -0,0 +1,60 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace System.Collections
+{
+   namespace Generic
+   {
+       public class DummyGeneric
+       {
+
+       }
+
+   }
+}
+
+namespace System
+{
+   namespace Net
+   {
+       public class DummyNet
+       {
+
+       }
+       namespace Http
+       {
+           public class DummyHttp
+           {
+
+           }
+
+       }
+   }
+}
+
+namespace System
+{
+   namespace Linq
+   {
+       public class DummyLinq
+       {
+
+       }
+
+   }
+}
+
+namespace System.Threading
+{
+   namespace Tasks
+   {
+       public class DummyTasks
+       {
+
+       }
+
+   }
+}
+
